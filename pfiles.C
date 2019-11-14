a9cfd8afb35e02165dcecaf7a5d513cc6fc07661
emorenoemb <emoreno.emb@gmail.com>
2019-11-13 18:33:47 -0600

    Update pfiles.C

--- pfiles.C
+++ pfiles.C
@@ -3,26 +3,11 @@
 void pfiles(const char *letters,const int number) {
 
    gROOT->Reset();
-   struct datasc_t {
-      Int_t id;
-      Int_t Energy;
-      Float_t id2;
-      Float_t px;
-      Float_t py;
-      Float_t pz;
-      Float_t pt;
-   };
-   datasc_t datasc;
-
-
 
    ifstream in;
-   //in.open("/Users/emoreno/Desktop/fast/data/fastC2pasameanpri00006.txt");
    in.open(letters);
-   //TCanvas* c1=new TCanvas("c1", "Signals", 600, 900);
    TTree *tree = new TTree("T","data from scintillator");
-   // create one branch with all information from the stucture
-   //tree->Branch("datasc",&datasc.id,"id/I:Energy:id2:px:py:pz:pt");
+   
    Int_t id;
    Float_t px,py,pz,penergia,pt,plamda;
 
@@ -38,8 +23,8 @@ void pfiles(const char *letters,const int number) {
    Int_t idd;
    Float_t pxx,pyy,pzz,penergia2,ptt,plamda2;
 
-   auto f = TFile::Open("Hex.root","RECREATE");
-//processing file of data
+   auto f = TFile::Open("Hexagon.root","RECREATE");
+
    while (!in.eof()) {
       in >> idd >> pxx >> pyy >> pzz >> penergia2 >> plamda2 >> ptt;
       if (!in.good()) break;
@@ -57,5 +42,6 @@ void pfiles(const char *letters,const int number) {
    //gSystem->Sleep( 10000 );
    tree->Print();
    tree->Write();
+   f.Close();
    //exit (0);
 }
